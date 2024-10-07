ea2e9ab8555a3a73bd050469400485bf4ec11d79
saskatrpkov <aleksandratrpkov1@gmail.com>
2024-10-07 11:52:45 +0200

    loginUsera i povezivanje sa bazom

---
+++ model/user.php
@@ -0,0 +1,22 @@
+<?php
+
+class User{
+    public $id;
+    public $username;
+    public $password;
+
+    public function __construct($id=null,$username=null,$password=null)
+    {
+        $this->id=$id;
+        $this->username=$username;
+        $this->password=$password;
+    }
+
+    public static function loginUser(User $user,mysqli $conn){
+        $query="SELECT * FROM user WHERE username='$user->username' AND password='$user->password'";
+        return $conn->query($query);
+    }
+}
+
+
+?>
